6af3d640b6de7178b5ceac7019fe35afe283fce2
Frances Morales <fmorales@francesmorales.com>
2014-08-21 21:02:40 -0700

    A basic script that spins up a DB, writes to it, then reads the data back out

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+	
+	gem 'rake'
+	gem 'minitest'
+
+
+	gem 'mongodb'	#remember to install mongo   brew install mongo
+	gem 'mongo_ext'
+	gem 'bson_ext'
+	gem 'json'
---
+++ monogodb_spec.rb
@@ -0,0 +1,42 @@
+
+require "minitest/autorun"
+require 'minitest/spec'
+require 'mongo'
+require 'bson'
+
+class Specs < MiniTest::Spec
+	before do
+		db = Mongo::Connection.new.db("dogs")
+		@dogs = db.collection('dogs')  
+	end
+
+	after do
+		@dogs.remove
+	end
+
+	describe "Do a thing" do
+		it "does a thing" do
+		end
+		it "does another thing" do
+		end
+	end	
+
+	describe "Lets write and return things from the database" do
+		it "returns the data we expect from the database" do
+			fido = {
+			name: "Fido",
+			species: "cocker spaniel",
+			gender: "male"
+			}
+			test_dog = {"name"=>"Fido"}
+			fido_id = @dogs.insert(fido)
+			#this test will fail if a new dog is entered with a name that comes before Fido
+			#where the Fi remains the same, such as Fickle.
+			puts @dogs.find({"name" => /Fi/}).first
+			our_dog = @dogs.find({"name" => /Fi/}).first.to_hash
+			test_dogName = test_dog["name"]
+			our_dogName = our_dog["name"]
+			assert test_dogName == our_dogName
+		end	
+	end
+end
